f960bf09b13e7f35c0f7c62b78b3147b6808b446
yunshin <2younsin@gmail.com>
2024-05-19 21:00:22 +0900

    ➕ eslint & prettier 설치 및  설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,40 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'standard-with-typescript',
+		'eslint:recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:react/recommended',
+		'plugin:prettier/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	plugins: ['react', '@typescript-eslint', 'jsx-a11y'],
+	rules: {
+		'@typescript-eslint/consistent-type-definitions': ['error', 'type'], // 타입 선언시, type-alias 사용
+		'@typescript-eslint/no-unused-vars': 'warn', // 사용하지 않는 변수 노란줄 경고
+		'react/react-in-jsx-scope': 'off', // "import React from 'react' 없어도 할 수 있도록
+		'@typescript-eslint/explicit-function-return-type': 'off', // 'on'인 경우, 함수에 return 타입 명시 강제
+		'@typescript-eslint/consistent-type-exports': 'error', // 'import type' 강제
+		'@typescript-eslint/consistent-type-imports': 'error', // 'export type' 강제
+	},
 }
